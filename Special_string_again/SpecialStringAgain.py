ea15707e2af41238ee9711460f54d069065e5d0c
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-25 18:19:48 -0800

    first commit

---
+++ Special_string_again/SpecialStringAgain.py
@@ -0,0 +1,24 @@
+def substrCount(n, s):
+    count = n
+    for i in range(0, n):
+        for j in range(i+2, n+1):
+            subStr = s[i:j]
+            if isSpecial(subStr):
+                print(subStr)
+                count += 1
+
+    return count
+
+
+def isSpecial(s):
+    if len(s) % 2 == 1:
+        halfSize = int(len(s) / 2)
+        sTemp = s[:halfSize] + s[halfSize + 1:]
+        s = sTemp
+
+    firstCh = s[0]
+    for ch in s:
+        if firstCh != ch:
+            return False
+
+    return True
