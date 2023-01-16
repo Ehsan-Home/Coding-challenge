6425cfa6a30884853f6da62e68d412829bdefbca
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-16 09:13:18 -0800

    enhanced version of bubble sort added

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,7 +1,7 @@
 # 11 7 3 12
 def bubble_sort(list):
     n = len(list)
-    for i in range(0, n):
+    for i in range(n):
         for j in range(0, n - i - 1):
             if list[j] > list[j+1]:
                 list[j], list[j+1] = list[j+1], list[j]
@@ -9,4 +9,15 @@ def bubble_sort(list):
     return list
 
 
-print(bubble_sort([12, 10, 4, -2, -4]))
+# Easier to understand
+def enhanced_bubble_sort(list):
+    for i in range(len(list) - 1, 0, -1):
+        for j in range(i):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+
+    return list
+
+
+# print(bubble_sort([12, 133, 10, 4, -2, -4]))
+print(enhanced_bubble_sort([12, 10, 4, -2, -4]))
