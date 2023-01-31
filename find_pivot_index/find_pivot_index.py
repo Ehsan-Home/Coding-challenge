62d8d166057915f98cd2116e1ffacd174b81e7d5
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-01-31 13:39:32 -0800

    cleaner approach added

--- find_pivot_index/find_pivot_index.py
+++ find_pivot_index/find_pivot_index.py
@@ -35,3 +35,37 @@ class Solution:
             i += 1
 
         return -1
+
+# Cleaner approach
+# [1, -1, 2]
+# leftArray = [0,1,0]
+# rightArray= [1,2,0]
+# [1,7,3,6,5,6]
+# leftArray = [0,1,8,11,17,22], sum = 0
+# rightArray= [0,0,0,0,0,0], sum = 0
+# rightArray= [0,0,0,11,5,0]
+# class Solution:
+#     def pivotIndex(self, nums: List[int]) -> int:
+#         leftArray = []
+#         rightArray= []
+
+#         Sum = 0
+#         leftArray.append(0)
+#         for i in range(1,len(nums)):
+#             Sum += nums[i-1]
+#             leftArray.append(Sum)
+
+#         Sum = 0
+#         rightArray.insert(0,0)
+#         for i in range(len(nums) - 2, -1 , -1):
+#             Sum += nums[i+1]
+#             rightArray.insert(0,Sum)
+
+#         i = 0
+#         while i < len(nums):
+#             if leftArray[i] == rightArray[i]:
+#                 return i
+
+#             i+=1
+
+#         return -1
