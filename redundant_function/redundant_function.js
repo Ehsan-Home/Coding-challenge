f4be8b918d340969ead84baf68a9c27633155c66
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-02-12 16:27:02 -0800

    redundant function

---
+++ redundant_function/redundant_function.js
@@ -0,0 +1,10 @@
+// You can return a function O_o
+// Link: https://edabit.com/challenge/hzxN9bAebBPNqdQto
+
+//function redundant(str) {
+//	return () => str;
+//}
+
+// function redundant(str) {
+// 	return function() {return str;}
+// }
